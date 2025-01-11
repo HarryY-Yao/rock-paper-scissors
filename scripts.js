263dfc09efbf99cb8e6571e2d9cfe0a0de21c258
HarryY-Yao <[200~184984708+HarryY-Yao@users.noreply.github.com>
2025-01-10 22:01:34 -0500

    Assign names to values

--- scripts.js
+++ scripts.js
@@ -4,7 +4,13 @@ function getComputerChoice() {
 
 
     let choice = Math.floor(Math.random() * (max_choice - min_choice + 1) + min_choice); // Calculates an integer between 1 and 3
-    console.log(choice);
-}
+    
+    if (choice == 1) {
+        choice = "Rock";
+    } else if (choice == 2) {
+        choice = "Paper";
+    } else {
+        choice = "Scissors";
+    }
 
-getComputerChoice();
+}
