4e78b2f6f42b5b58d591bcab7041a975bd95d63d
HarryY-Yao <[200~184984708+HarryY-Yao@users.noreply.github.com>
2025-01-10 22:31:36 -0500

    Create playRound function

--- scripts.js
+++ scripts.js
@@ -6,19 +6,65 @@ function getComputerChoice() {
     let choice = Math.floor(Math.random() * (max_choice - min_choice + 1) + min_choice); // Calculates an integer between 1 and 3
     
     if (choice == 1) {
-        choice = "Rock";
+        choice = "rock";
     } else if (choice == 2) {
-        choice = "Paper";
+        choice = "paper";
     } else {
-        choice = "Scissors";
+        choice = "scissors";
     }
-
+    return choice;
 }
 
 
 function getHumanChoice() {
     let choice = prompt("Please Enter your choice (Rock, Paper, or Scissors): ");
-    return choice;
+    return choice.toLowerCase();
 }
 
-console.log(getHumanChoice());
+
+humanScore = 0
+computerScore = 0
+
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == "scissors") {
+        if (computerChoice == "scissors") {
+            console.log("It's a tie! Scissors negates scissors");
+        } else if (computerChoice == "paper") {
+            console.log("You win! Scissors beats paper");
+            humanScore++;
+        } else {
+            console.log("You lose! Rock beats scissors");
+            computerChoice++;
+        }
+    } else if (humanChoice == "paper") {
+        if (computerChoice == "paper") {
+            console.log("It's a tie! Paper negates paper");
+        } else if (computerChoice == "rock") {
+            console.log("You win! paper beats rock");
+            humanScore++;
+        } else {
+            console.log("You lose! Scissors beats paper");
+            computerChoice++;
+        }
+    } else if (humanChoice == "rock") {
+        if (computerChoice == "rock") {
+            console.log("It's a tie! Rock negates rock");
+        } else if (computerChoice == "scissors") {
+            console.log("You win! Rock beats scissors");
+            humanScore++;
+        } else {
+            console.log("You lose! Paper beats rock");
+            computerChoice++;
+        }
+
+    } else {
+        console.log("Invalid input");
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
